df9cd7caf904e547a15107bb3fffb241f38cc4f4
damasukma <damasukmath@gmail.com>
2022-11-16 12:01:30 +0700

    fix padding dan font

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,9 @@
 function Header() {
   return (
-    <header className="container flex text-center mx-auto py-10 px-4">
+    <header className="container flex text-center mx-auto py-10 ">
       <div className="md:w-4/6 mx-auto">
         <h1 className="text-xl md:text-2xl font-bold">Netra Community</h1>
-        <p className="font-thin md:px-4">Netra adalah komunitas dibawah naungan TJKT SMKN 1 Adiwerna yang berfokus di bidang Teknologi Informasi. Netra bertujuan untuk mewadahi dan mengembangkan skill siswa - siswi SMKN 1 Adiwerna dalam ranah Teknologi Informasi.</p>
+        <p className="font-sans md:px-4">Netra adalah komunitas dibawah naungan TJKT SMKN 1 Adiwerna yang berfokus di bidang Teknologi Informasi. Netra bertujuan untuk mewadahi dan mengembangkan skill siswa - siswi SMKN 1 Adiwerna dalam ranah Teknologi Informasi.</p>
       </div>
     </header>
   );
