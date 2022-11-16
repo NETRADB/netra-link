7d9e95f69eafd4f65947f9cf7c90152fb2bb7802
damasukma <damasukmath@gmail.com>
2022-11-16 10:54:37 +0700

    init

---
+++ components/FooterDisclaimer.js
@@ -0,0 +1,12 @@
+import DonateButton from "./DonateButton";
+import packageJson from '../package.json';
+
+function FooterDisclaimer() {
+  return (
+    <div className="text-xs mt-8">
+      <p className="">Netra Community @2022</p>
+    </div>
+  );
+}
+
+export default FooterDisclaimer;
---
+++ components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="container flex text-center mx-auto py-10 px-4">
+      <div className="md:w-4/6 mx-auto">
+        <h1 className="text-xl md:text-2xl font-bold">Netra Community</h1>
+        <p className="font-thin">Netra adalah komunitas dibawah naungan TJKT SMKN 1 Adiwerna yang berfokus di bidang Teknologi Informasi. Netra bertujuan untuk mewadahi dan mengembangkan skill siswa - siswi SMKN 1 Adiwerna dalam ranah Teknologi Informasi.</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
